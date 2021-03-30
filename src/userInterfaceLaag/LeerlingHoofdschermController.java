809decde87051582bf014ed7b1a4ecebde0cb250
Jens Rijks <jens.rijks@student.hu.nl>
2021-03-30 13:27:58 +0200

    Commit 30-03 logout

--- src/userInterfaceLaag/LeerlingHoofdschermController.java
+++ src/userInterfaceLaag/LeerlingHoofdschermController.java
@@ -3,11 +3,16 @@ package userInterfaceLaag;
 import domeinLaag.Student;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class LeerlingHoofdschermController {
 
@@ -24,6 +29,18 @@ public class LeerlingHoofdschermController {
     }
 
     public void loguitEnSluiten(ActionEvent actionEvent) {
-        System.exit(0);
+        try {
+            ((Node)actionEvent.getSource()).getScene().getWindow().hide();
+            Stage primaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = loader.load(getClass().getResource("/userInterfaceLaag/LoginScherm.fxml"));
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
