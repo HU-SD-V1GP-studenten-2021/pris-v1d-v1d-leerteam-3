6847529d1cf97afbe5c6187117ef31034ffe04a6
LuukVanHeck <luuk.vanheck@student.hu.nl>
2021-03-31 14:20:36 +0200

    Login werkt via pull jens

--- src/userInterfaceLaag/DocentenSchermController.java
+++ src/userInterfaceLaag/DocentenSchermController.java
@@ -1,7 +1,11 @@
 package userInterfaceLaag;
 
 import domeinLaag.Docent;
+import domeinLaag.Klas;
+import domeinLaag.Les;
 import domeinLaag.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,24 +14,69 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import javax.print.Doc;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class DocentenSchermController {
     @FXML private Rectangle rectangleBoven;
     @FXML private Button loguitKnop;
     @FXML public Label naamLabel;
+    @FXML private TableView tableView2;
+    @FXML private TableColumn<Klas, String> klasid;
+    @FXML private TableColumn<Les, String> datumid;
+    @FXML private TableColumn<Les, String> lesid;
 
-    private Docent docent = Docent.getAccount();
+    @FXML private TableView tableView1;
+    @FXML private TableColumn<Student, String> naamid;
+    @FXML private TableColumn<Student, String> studentid;
+    @FXML private TableColumn<Student, String> emailid;
+    @FXML private TableColumn<Student, String> rollcall;
+    @FXML private TableColumn<Student, String> aanwezigid;
+    @FXML private TableColumn<Student, String> infoid;
 
-    public void initialize() {
+        private Docent docent = Docent.getAccount();
+
+    public void initialize() throws Exception {
+        int nummer = docent.getMedewerkersnummer();
+        String url = "jdbc:postgresql://localhost/SDGP";
+        Properties props = new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","Galaxy");
+        Connection conn = DriverManager.getConnection(url, props);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select lesnummer, klasnaam, datum from les\n" +
+                "join docent on docent.docentnummer = les.docentdocentnummer\n" +
+                "where docentnummer = " + nummer);
+
+        ObservableList<String> data = FXCollections.observableArrayList();
+        while (rs.next()) {
+            String lesnummer = rs.getString("lesnummer");
+            String klasnaam = rs.getString("klasnaam");
+            String datum = rs.getString("datum");
+            data.addAll(lesnummer,klasnaam,datum);
+        }
+
         String s = docent.getNaam();
         naamLabel.setText(s);   // in de klasse domeinLaag.Student de naam opvragen
+        klasid.setCellValueFactory(new PropertyValueFactory<>("klasnaam"));
+        datumid.setCellValueFactory(new PropertyValueFactory<>("datum"));
+        lesid.setCellValueFactory(new PropertyValueFactory<>("lesnummer"));
+        System.out.println(data);
+        tableView2.setItems(data);
     }
+
 
     public void loguitEnAfsluiten(ActionEvent actionEvent){
         try {
