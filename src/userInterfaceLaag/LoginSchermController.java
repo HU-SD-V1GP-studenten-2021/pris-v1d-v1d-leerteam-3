a0e0ec66e6a3e9d5b7a3fb064d086f06c67c7ae8
LuukVanHeck <luuk.vanheck@student.hu.nl>
2021-04-01 11:58:10 +0200

    ...

--- src/userInterfaceLaag/LoginSchermController.java
+++ src/userInterfaceLaag/LoginSchermController.java
@@ -35,10 +35,10 @@ public class LoginSchermController {
         String naam = naamVeld.getText();
         String wachtwoord = wachtwoordVeld.getText();
         Stage loginscherm = (Stage) loginKnop.getScene().getWindow();
-        String url = "jdbc:postgresql://localhost/GP";
+        String url = "jdbc:postgresql://localhost/SDGP";
         Properties props = new Properties();
         props.setProperty("user","postgres");
-        props.setProperty("password","ruben");
+        props.setProperty("password","Galaxy");
         Connection conn = DriverManager.getConnection(url, props);
 
         if(!naam.contains("@student.hu.nl") &&!naam.contains("@hu.nl")){
