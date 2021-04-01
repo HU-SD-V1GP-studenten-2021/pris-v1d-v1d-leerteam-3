0ce40a2ab3d82a6feb953ca9168e707a0ca28c3a
LucaJ <Luca.Franken@student.hu.nl>
2021-04-01 11:41:17 +0200

    datepicker

--- src/userInterfaceLaag/LeerlingHoofdschermController.java
+++ src/userInterfaceLaag/LeerlingHoofdschermController.java
@@ -6,6 +6,7 @@ import domeinLaag.Student;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Side;
@@ -25,10 +26,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Date;
 
 public class LeerlingHoofdschermController {
 
 
+    public DatePicker datepickerid;
     @FXML private Button loguitKnop;
     @FXML private Label naamLabel;
     @FXML private TableView aanwezigheidsTabel;
@@ -36,23 +39,25 @@ public class LeerlingHoofdschermController {
     @FXML private TableColumn<Klas, String> datumid;
     @FXML private TableColumn<Klas, String> docentid;
     @FXML private TableColumn<Klas, String> tijdid;
-    @FXML private TableColumn<Klas, CheckBox> aanwezigid;
+    @FXML private TableColumn<Klas, String> aanwezigid;
     @FXML private PieChart rollCallAttendance;
 
     private Student student = Student.getAccount();
 
 
     public void initialize() {
+        datepickerid.setValue(LocalDate.now());
         String s = student.getNaam();
         naamLabel.setText(s);   // in de klasse domeinLaag.Student de naam opvragen
-
         lesid.setCellValueFactory(new PropertyValueFactory<>("lesnummer"));
         datumid.setCellValueFactory(new PropertyValueFactory<>("datum"));
         docentid.setCellValueFactory(new PropertyValueFactory<>("docent"));
         tijdid.setCellValueFactory(new PropertyValueFactory<>("begintijd"));
-        aanwezigid.setCellValueFactory(new PropertyValueFactory<>("check"));
+//        aanwezigid.setCellValueFactory(new PropertyValueFactory<>("aanwezigheid"));
+        aanwezigheidsTabel.setEditable(true);
 
         aanwezigheidsTabel.setItems(getLessen());
+
 
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
         new PieChart.Data("Aanwezig " + student.getRollCall() + "%", student.getRollCall()),
@@ -68,7 +73,6 @@ public class LeerlingHoofdschermController {
     public ObservableList<Les> getLessen(){
         ObservableList<Les> lessen = FXCollections.observableArrayList();
         lessen.addAll(student.getKlas().getLessen());
-        System.out.println(lessen);
         return lessen;
     }
 
@@ -93,17 +97,34 @@ public class LeerlingHoofdschermController {
 
     public void popUpScherm(MouseEvent mouseEvent) throws IOException {
 
+
         Les les = (Les) aanwezigheidsTabel.getSelectionModel().getSelectedItem();
         int lesnummer = les.getLesnummer();
-        System.out.println(lesnummer);
+        if(!les.getDatum().isBefore(LocalDate.now())){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AfmeldenScherm.fxml"));
+            Parent root = loader.load();
+            Stage newStage = new Stage();
+            newStage.setTitle("Afmelden");
+            newStage.setScene(new Scene(root));
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.showAndWait();
+            initialize();
+        }
+    }
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("AfmeldenScherm.fxml"));
-        Parent root = loader.load();
-        Stage newStage = new Stage();
-        newStage.setTitle("Afmelden");
-        newStage.setScene(new Scene(root));
-        newStage.initModality(Modality.APPLICATION_MODAL);
-        newStage.showAndWait();
-        initialize();
+    public void getdatum(Event event) throws NullPointerException {
+        LocalDate datum = datepickerid.getValue();
+        aanwezigheidsTabel.setItems(setLessen(datum));
+
+    }
+
+    public ObservableList<Les> setLessen(LocalDate datum){
+        ObservableList<Les> lessen = FXCollections.observableArrayList();
+        for(Les les : student.getKlas().getLessen()){
+            if (les.getDatum().isAfter(datepickerid.getValue())){
+                lessen.add(les);
+            }
+        }
+        return lessen;
     }
 }
