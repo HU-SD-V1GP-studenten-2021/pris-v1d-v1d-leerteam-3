dfb91c1b3a4746519121ff1cf6825ac1e3d79468
Jens Rijks <jens.rijks@student.hu.nl>
2021-04-02 13:27:06 +0200

    g

--- src/userInterfaceLaag/LoginSchermController.java
+++ src/userInterfaceLaag/LoginSchermController.java
@@ -26,9 +26,6 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
 
 public class LoginSchermController {
     @FXML private Button loginKnop;
@@ -39,51 +36,6 @@ public class LoginSchermController {
     private Student account = Student.getAccount();
     private Object Klas;
 
-    public class SendEmail {
-
-        public static void main(String [] args) {
-            // Recipient's email ID needs to be mentioned.
-            String to = "abcd@gmail.com";
-
-            // Sender's email ID needs to be mentioned
-            String from = "web@gmail.com";
-
-            // Assuming you are sending email from localhost
-            String host = "localhost";
-
-            // Get system properties
-            Properties properties = System.getProperties();
-
-            // Setup mail server
-            properties.setProperty("mail.smtp.host", host);
-
-            // Get the default Session object.
-            Session session = Session.getDefaultInstance(properties);
-
-            try {
-                // Create a default MimeMessage object.
-                MimeMessage message = new MimeMessage(session);
-
-                // Set From: header field of the header.
-                message.setFrom(new InternetAddress(from));
-
-                // Set To: header field of the header.
-                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-                // Set Subject: header field
-                message.setSubject("This is the Subject Line!");
-
-                // Now set the actual message
-                message.setText("This is actual message");
-
-                // Send message
-                Transport.send(message);
-                System.out.println("Sent message successfully....");
-            } catch (MessagingException mex) {
-                mex.printStackTrace();
-            }
-        }
-    }
 
     public void loginAccount(ActionEvent actionEvent) throws Exception  {
         String naam = naamVeld.getText();
